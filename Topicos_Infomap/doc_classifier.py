d8eea8a88fc450ab461b192777df39257e3b91c3
Lucas Rocha <lucas.elias125@gmail.com>
2019-02-22 11:39:45 -0300

    Classificando documentos

--- Topicos_Infomap/doc_classifier.py
+++ Topicos_Infomap/doc_classifier.py
@@ -14,19 +14,25 @@ for doc in os.listdir():
     f.close()
 
     n_cluster = 0
-    cluster = []
+    cluster = [] # cada elemento i da lista contém o conjunto de pares termo-flow do cluster i
+    flow_cluster = [] # cada elemento i da lista contém o flow total do cluster i
+    flow_sum = 0
 
     for l in lines[2:]:
         if l[0] == '*':
             break
 
         spl = l.split()
-        if int(spl[0][0]) == n_cluster:
-            cluster[n_cluster-1].append(spl[2][1:-1])
+        if int(spl[0].split(':')[0]) == n_cluster:
+            cluster[n_cluster-1][spl[2][1:-1]] = float(spl[1])
+            flow_sum += float(spl[1]) 
         else:
             n_cluster += 1
-            cluster.append([spl[2][1:-1]])
-
+            cluster.append({spl[2][1:-1]:float(spl[1])})
+            if not n_cluster == 1:
+                flow_cluster.append(flow_sum)
+            flow_sum = float(spl[1])
+    flow_cluster.append(flow_sum)
     #--------- Carregando documentos ----------#
     ego = doc.split('.')[0]
     file = doc_folder + ego + '.txt'
@@ -42,10 +48,17 @@ for doc in os.listdir():
         termos = doc.split()
         for topic in range(0, n_cluster):
             count = 0
+            flow = 0
             for t in termos:
-                if t in cluster[topic]:
-                    count+=1
-            k = count/len(cluster[topic])
+                if t in cluster[topic].keys():
+                    count += 1
+                    flow += cluster[topic][t]
+            if flow_cluster[topic] == 0 and count > 0:
+                k = 100
+            elif flow_cluster[topic] == 0:
+                k = 0.0
+            else:
+                k = count * flow/flow_cluster[topic]
             output.write(str(k) + ' ')
         output.write('\n')
     
